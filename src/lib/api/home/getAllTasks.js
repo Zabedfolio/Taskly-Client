94fff3b9f80a1a40cc0d0e4ac7a87fd889d76b52
Zabedfolio <zabedfolio@gmail.com>
2026-06-27 00:28:45 +0600

    added

--- src/lib/api/home/getAllTasks.js
+++ src/lib/api/home/getAllTasks.js
@@ -2,5 +2,5 @@ const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 export const getAllTasks = async () => {
   const res = await fetch(`${BASE_URL}/api/tasks`);
-  return res.json();
+  return res.json()
 };
